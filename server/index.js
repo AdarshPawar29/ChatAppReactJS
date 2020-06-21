503baf6e657f552db9017d6b23d68936342af7d3
Adarsh Pawar <adarshpawar@Adarshs-MacBook-Air.local>
2020-06-21 14:55:17 +0530

    socket.io done

--- server/index.js
+++ server/index.js
@@ -5,10 +5,18 @@ const http = require("http");
 const PORT = process.env.PORT || 5000;
 
 const router = require("./router");
+const { Socket } = require("dgram");
 
 const app = express();
 const server = http.createServer(app);
 const io = sockectio(server);
+
+io.on("connection", (socket) => {
+  console.log("we have a new connection!!");
+  socket.on("disconnect", () => {
+    console.log("User had left!!");
+  });
+});
 
 app.use(router);
 
